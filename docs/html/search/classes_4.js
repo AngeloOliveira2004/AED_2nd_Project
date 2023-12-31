302b42c70ce2da5e784439045daa8ba4458bea6b
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2023-12-31 16:01:08 +0000

    Documentation finished

--- docs/html/search/classes_4.js
+++ docs/html/search/classes_4.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['loadfiles_0',['LoadFiles',['../classLoadFiles.html',1,'']]],
-  ['loadingfunctions_1',['LoadingFunctions',['../classLoadingFunctions.html',1,'']]],
-  ['logic_2',['Logic',['../classLogic.html',1,'']]]
+  ['loadingfunctions_0',['LoadingFunctions',['../classLoadingFunctions.html',1,'']]],
+  ['logic_1',['Logic',['../classLogic.html',1,'']]]
 ];
